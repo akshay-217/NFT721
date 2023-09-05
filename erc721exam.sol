26da35d8d688559879b54d540099d57c49f386e1
akshay-217 <120545815+akshay-217@users.noreply.github.com>
2023-09-05 10:15:43 +0530

    Update erc721exam.sol

--- erc721exam.sol
+++ erc721exam.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; and 
+import "@openzeppelin/contracts/access/Ownable.sol"; and
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CustomERC721 is ERC721Enumerable, Ownable{
